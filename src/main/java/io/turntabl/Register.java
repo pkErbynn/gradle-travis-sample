e3714dd9d2a4c99efe221b793106895802bacf40
erbynn <john.erbynn@gmail.com>
2019-11-10 19:24:53 +0000

    level counting

--- src/main/java/io/turntabl/Register.java
+++ src/main/java/io/turntabl/Register.java
@@ -1,6 +1,7 @@
 package io.turntabl;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Register {
@@ -14,11 +15,27 @@ public class Register {
         return clients;
     }
 
-    public List<String> getNamesOfGoldClients(){
+    public List<String> getNamesOfGoldClients() {
         return clients.stream()
                 .filter(c -> c.getServiceLevel().equals(ServiceLevel.GOLD))
                 .map(Client::getName)
                 .collect(Collectors.toList());
     }
+
+    public Map<Integer, String> countClientAtEveryLevel() {
+        Map<>
+        int goldCount = ((int) clients.stream()
+                .filter(c -> c.getServiceLevel() == ServiceLevel.GOLD)
+                .count());
+        int goldPlatinum = ((int) clients.stream()
+                .filter(c -> c.getServiceLevel() == ServiceLevel.PLATINUM)
+                .count());
+        int premiumCount = ((int) clients.stream()
+                .filter(c -> c.getServiceLevel() == ServiceLevel.PREMIUM)
+                .count());
+
+    }
+
+
 }
 
